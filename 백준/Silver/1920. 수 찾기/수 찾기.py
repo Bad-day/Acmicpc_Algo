5b416f0698c660d5b0ef001a4db3fc11da393a65
Bad-day <75732202+Bad-day@users.noreply.github.com>
2023-05-17 23:05:28 +0900

    [Silver IV] Title: 수 찾기, Time: 172 ms, Memory: 50400 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,5 @@
+import sys; input=sys.stdin.readline
+n = int(input());a = set(map(int, input().split()))
+m = int(input());arr = list(map(int, input().split()))
+for i in arr:
+	print(1) if i in a else print(0)
